1da047a8279bf59707f4ed02c066d2dd63f0a9a4
soraia.ssouza2 <soraia.ssouza2@ITQ0675982W10-1>
2024-07-30 16:47:40 -0300

    Adicionar arquivos de projeto.

---
+++ meuApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace meuApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int i;
+            Console.WriteLine("Digite um número para a tabuada");
+            numero = int.Parse(Console.ReadLine());
+            for (i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(numero + " X " + i + " = " + (numero * i));
+            }
+        }
+    }
+}
